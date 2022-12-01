1bbae45b8aa3bbb388d9fde0a68618b1f4afe6e0
alex merecka <merecka@gmail.com>
2022-12-01 12:46:36 -0600

    Score is adding with dice image clicks.

--- farkle.js
+++ farkle.js
@@ -1,6 +1,17 @@
 var diceArr = [];
+var diceBankedScore = {
+  1: 0,
+  2: 0,
+  3: 0,
+  4: 0,
+  5: 0,
+  6: 0,
+};
+var totalScore = 0;
 
 function initializeDice() {
+  document.getElementById("totalScore").innerHTML = totalScore;
+
   for (i = 0; i < 6; i++) {
     diceArr[i] = {};
     diceArr[i].id = "die" + i + 1;
@@ -34,8 +45,76 @@ function diceClick(img) {
 
   img.classList.toggle("transparent");
   if (diceArr[i].clicked === 0) {
-    diceArr[i].clicked == 1;
+    diceArr[i].clicked = 1;
+    scoreDice(i);
   } else {
-    diceArr[i].clicked == 0;
+    diceArr[i].clicked = 0;
+    takeAwayDice(i);
   }
+  //   console.log(diceArr);
+}
+
+function scoreDice(diceDataNumber) {
+  diceBankedScore[diceArr[diceDataNumber].value]++;
+  calculateTotalScore(diceDataNumber);
+}
+
+function takeAwayDice(diceDataNumber) {
+  diceBankedScore[diceArr[diceDataNumber].value]--;
+  calculateTotalScore(diceDataNumber);
+}
+
+function calculateTotalScore(diceDataNumber) {
+  console.log(diceBankedScore);
+  console.log(typeof diceDataNumber);
+  var valueKey = parseInt(diceDataNumber) + 1;
+  console.log("valueKey is " + valueKey);
+  switch (valueKey) {
+    case 1:
+      if (diceBankedScore[valueKey] === 1) {
+        totalScore = totalScore + 100;
+      } else if (diceBankedScore[valueKey] === 2) {
+        totalScore = totalScore + 100;
+      } else if (diceBankedScore[valueKey] === 3) {
+        totalScore = totalScore + 1000;
+      }
+      break;
+
+    case 2:
+      if (diceBankedScore[valueKey] === 3) {
+        totalScore = totalScore + 200;
+      }
+      break;
+
+    case 3:
+      if (diceBankedScore[valueKey] === 3) {
+        totalScore = totalScore + 300;
+      }
+      break;
+
+    case 4:
+      if (diceBankedScore[valueKey] === 3) {
+        totalScore = totalScore + 400;
+      }
+      break;
+
+    case 5:
+      if (diceBankedScore[valueKey] === 1) {
+        totalScore = totalScore + 50;
+      } else if (diceBankedScore[valueKey] === 2) {
+        totalScore = totalScore + 50;
+      } else if (diceBankedScore[valueKey] === 3) {
+        totalScore = totalScore + 500;
+      }
+      break;
+
+    case 6:
+      if (diceBankedScore[valueKey] === 3) {
+        totalScore = totalScore + 600;
+      }
+      break;
+  }
+
+  console.log("totalScore is " + totalScore);
+  document.getElementById("totalScore").innerHTML = totalScore;
 }
